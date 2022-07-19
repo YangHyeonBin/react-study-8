47fb57a052070e77d8d12b1e138b52cae590ee2b
ekil <idgusqls0506@gmail.com>
2022-07-19 15:44:03 +0900

    스타워즈 api 이용해 http 요청

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]); // 빈 배열
+
+  function fetchMoviesHandler() {
+    fetch('https://swapi.dev/api/films/').then(response => { // reponse는 요청 응답 데이터 담긴 객체
+      return response.json(); // response(=Promise?) 객체의 내장 메소드, json 형태 데이터 -> JS object
+    }).then(data => {
+      const transformedMovies = data.results.map(movieData => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        }; // http 요청으로 받는 데이터와, 원하는 데이터 이름이 다를 때 이런 식으로 변환 가능
+      });
+      setMovies(transformedMovies);
+    });
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>  
   );
