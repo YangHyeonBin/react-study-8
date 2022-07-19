823eaed6f983c6ab0590f7d7107b58d6a2ce7ede
ekil <idgusqls0506@gmail.com>
2022-07-19 15:56:02 +0900

    .then 대신 async await 사용

--- src/App.js
+++ src/App.js
@@ -6,20 +6,19 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]); // 빈 배열
 
-  function fetchMoviesHandler() {
-    fetch('https://swapi.dev/api/films/').then(response => { // reponse는 요청 응답 데이터 담긴 객체
-      return response.json(); // response(=Promise?) 객체의 내장 메소드, json 형태 데이터 -> JS object
-    }).then(data => {
-      const transformedMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        }; // http 요청으로 받는 데이터와, 원하는 데이터 이름이 다를 때 이런 식으로 변환 가능
-      });
-      setMovies(transformedMovies);
+  async function fetchMoviesHandler() {
+    const response = await fetch('https://swapi.dev/api/films/')
+    const data = await response.json();
+
+    const transformedMovies = data.results.map(movieData => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      }; // http 요청으로 받는 데이터와, 원하는 데이터 이름이 다를 때 이런 식으로 변환 가능
     });
+    setMovies(transformedMovies);
   }
 
   return (
@@ -30,7 +29,7 @@ function App() {
       <section>
         <MoviesList movies={movies} />
       </section>
-    </React.Fragment>  
+    </React.Fragment>
   );
 }
 
